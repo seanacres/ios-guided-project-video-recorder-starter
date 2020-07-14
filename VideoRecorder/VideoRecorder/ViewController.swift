7707a51db321e59fd0ea2cea6ba93ddf0c35fc87
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-13 21:59:22 -0400

    Added recording permission requesting

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -17,12 +18,36 @@ class ViewController: UIViewController {
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		
-		// TODO: get permission
-		
-		showCamera()
-		
+
+		requestPermissionAndShowCamera()
 	}
+    
+    private func requestPermissionAndShowCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            requestVideoPermission()
+        case .restricted:
+            preconditionFailure("Video is disabled")
+        case .denied:
+            preconditionFailure("Tell the user they need to grant permissions")
+        case .authorized:
+            showCamera()
+        @unknown default:
+            preconditionFailure("A new status code was added that was not handled")
+        }
+    }
+    
+    private func requestVideoPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (isGranted) in
+            guard isGranted else {
+                preconditionFailure("Access not granted")
+            }
+            
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
