cae85c1f0bde62519e1288123f1e37fa7cfa1778
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-13 22:21:54 -0400

    Configured session preset and output

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -40,7 +40,13 @@ class CameraViewController: UIViewController {
         
         captureSession.addInput(cameraInput)
         
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
         captureSession.commitConfiguration()
+        
+        cameraView.session = captureSession
     }
     
     private func bestCamera() -> AVCaptureDevice {
